7890e5b50f7ebf8abf9486ad0e66bfe570ec38d4
Floyd May <floyd@canyon-trail.com>
2023-04-10 15:41:33 -0500

    properly wire up DI to custom document executer

--- OttoTheGeek/Internal/OttoExecutionStrategy.cs
+++ OttoTheGeek/Internal/OttoExecutionStrategy.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using GraphQL;
+using GraphQL.DI;
 using GraphQL.Execution;
+using GraphQL.Validation;
 using GraphQLParser.AST;
 
 namespace OttoTheGeek.Internal
@@ -7,6 +10,16 @@ namespace OttoTheGeek.Internal
     // this class overrides the default behavior to run queries in parallel
     public sealed class OttoDocumentExecuter : DocumentExecuter
     {
+        public OttoDocumentExecuter(IDocumentBuilder documentBuilder,
+            IDocumentValidator documentValidator,
+            IExecutionStrategySelector executionStrategySelector,
+            IEnumerable<IConfigureExecution> configurations
+            )
+            : base(documentBuilder, documentValidator, executionStrategySelector, configurations)
+        {
+            
+        }
+        
         protected override IExecutionStrategy SelectExecutionStrategy(ExecutionContext context)
         {
             if(context.Operation.Operation == OperationType.Query)
